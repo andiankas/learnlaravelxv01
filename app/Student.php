57e8cb0547db0cdfbd3cb3949e29025ec577dcd1
andiankas <rustiantoandi17@gmail.com>
2019-09-15 23:24:56 +0700

    soft delete

--- app/Student.php
+++ app/Student.php
@@ -3,10 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Student extends Model
 {
     //
+    use SoftDeletes;
     protected $table = 'tbl_student';
     protected $fillable = ['nama','nip','email','jurusan'];
 }
